de750d90fd75c6ef7fbd7d7c2dd646dbfc76fd47
ChatCode <chatcode@MacBook-Pro-de-ChatCode.local>
2022-02-09 10:57:00 +0100

    sans les com

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-// const H1 = document.querySelector("h1");
-
-// fetch(`../test.json`)
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((data) => {
-//     console.log(data[0]);
-//     H1.innerHTML = `
-//     ${data[0].APIKEY}
-//     `;
-
-//     fetch(
-//       `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Paris,IL&key=${data[0].APIKEY}&inputtype=textquery&fields=name,photos`
-//     )
-//       .then((response) => {
-//         return response.json();
-//       })
-//       .then((dataPosition) => {
-//         console.log(data[0].APIKEY);
-//       });
-//   });
-
 let myHeaders = new Headers();
 
 let myInit = {
